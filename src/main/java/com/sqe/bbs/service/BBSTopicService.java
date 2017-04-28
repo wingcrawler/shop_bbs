b880bcddc6dcd4a4825700e160f5de8920f2fbf6
Andy <winglive@163.com>
2017-04-28 16:54:13 +0800

    add bbs service

---
+++ src/main/java/com/sqe/bbs/service/BBSTopicService.java
@@ -0,0 +1,18 @@
+/*   
+ * Copyright (c) 2014-2034 360taihe Ltd. All Rights Reserved.   
+ *   
+ * This software is the confidential and proprietary information of   
+ * 360taihe. You shall not disclose such Confidential Information.   
+ */
+
+/**
+ * 
+ */
+package com.sqe.bbs.service;
+
+import com.sqe.shop.service.AdapterService;
+import com.sqe.shop.service.BaseService;
+
+public class BBSTopicService extends AdapterService implements BaseService {
+
+}
