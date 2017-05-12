7d444a06a98cc6709fa225b409fb9b2a7e1dad70
dulz1991 <1025406812@qq.com>
2017-05-12 23:52:49 +0800

    update

--- src/main/java/com/sqe/shop/application/Application.java
+++ src/main/java/com/sqe/shop/application/Application.java
@@ -3,6 +3,8 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -12,10 +14,16 @@ import org.springframework.context.annotation.ImportResource;
 @ComponentScan(basePackages = "com.sqe.shop")  
 @EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(Application.class);
 	}
 
 }
