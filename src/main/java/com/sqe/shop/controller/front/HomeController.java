539a8e55ff166e9466da5e8bbf59833dcae5b758
dulz1991 <1025406812@qq.com>
2017-08-20 23:20:39 +0800

    移动端 首页数据接口

--- src/main/java/com/sqe/shop/controller/front/HomeController.java
+++ src/main/java/com/sqe/shop/controller/front/HomeController.java
@@ -21,6 +21,7 @@ import com.sqe.shop.service.ImageService;
 import com.sqe.shop.service.NewsService;
 import com.sqe.shop.service.ProductService;
 import com.sqe.shop.service.ProductTypeService;
+import com.sqe.shop.service.biz.BizHomeService;
 import com.sqe.shop.service.cached.CachedService;
 import com.sqe.shop.util.PageUtil;
 import com.sqe.shop.util.RegularUtil;
@@ -44,6 +45,9 @@ public class HomeController extends BaseFrontController {
 	@Autowired
 	private NewsService newsService;
 	
+	@Autowired
+	private BizHomeService bizHomeService;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView index(ModelAndView model) {
 		model.setViewName("index");
@@ -57,44 +61,7 @@ public class HomeController extends BaseFrontController {
 	 */
 	@RequestMapping(value="shopIndex", method = RequestMethod.GET)
 	public ModelAndView shopIndex(ModelAndView model) {
-		//轮播图
-		Advertisement advertisement = new Advertisement();
-		advertisement.setType(0);
-		PageUtil<Advertisement> adPage = advertisementService.getBeanListByParm(advertisement, 1, -1);
-		model.addObject("adList", adPage.getList());
-		
-		//8个商品
-		Product product = new Product();
-		product.setProductStatus(1);
-		PageUtil<Map<String, Object>> hotProductPage = productService.getHotProductList(product, 1, 8); 
-		model.addObject("hotProductList", hotProductPage.getList());
-		
-		//3个新闻资讯
-		PageUtil<Map<String, Object>> newPage = newsService.getMapListByParm(new News(), 1, 3);
-		if(newPage.getList()!=null && !newPage.getList().isEmpty()){
-			String newsTitle = "";
-			String newsContent = "";
-			for(Map<String, Object> map : newPage.getList()){
-				newsTitle = map.get("newsTitle").toString();
-				if(newsTitle.length()>10){
-					map.put("newsShortTitle", newsTitle.substring(0, 9)+"...");
-				} else {
-					map.put("newsShortTitle", newsTitle);
-				}
-				
-				newsContent = map.get("newsContent").toString();
-				newsContent = RegularUtil.Html2Text(newsContent);
-				if(newsContent.length()>100){
-					newsContent = newsContent.substring(0,100) + "...";
-					map.put("newsContent", newsContent);
-				} else {
-					map.put("newsContent", newsContent);
-				}
-				
-			}
-		}
-		model.addObject("newsList", newPage.getList());
-		
+		model.addAllObjects(bizHomeService.getHomeIndexData());
 		model.setViewName("shop/index");
 		return model;
 	}
--- src/main/java/com/sqe/shop/controller/mobile/MobileHomeController.java
+++ src/main/java/com/sqe/shop/controller/mobile/MobileHomeController.java
@@ -2,21 +2,27 @@ package com.sqe.shop.controller.mobile;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sqe.shop.controller.base.BaseFrontController;
+import com.sqe.shop.service.biz.BizHomeService;
 
 @Controller
 @RequestMapping("/h5")
 public class MobileHomeController extends BaseFrontController {
+	
+	@Autowired
+	private BizHomeService bizHomeService;
 
 	@ResponseBody
 	@RequestMapping("/index")
 	public Map<String, Object> index(ModelAndView model) {
-		return this.responseOK1("");
+		
+		return bizHomeService.getHomeIndexData();
 	}
 	
 }
