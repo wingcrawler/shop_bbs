ec518660b73e554fce43f0854fc8c9fd4369e049
Andy <winglive@163.com>
2017-08-22 23:03:19 +0800

    增加DTO 层

---
+++ src/main/java/com/sqe/shop/dto/PostDto.java
@@ -0,0 +1,15 @@
+package com.sqe.shop.dto;
+
+import java.util.Date;
+
+import com.sqe.shop.model.Post;
+
+public class PostDto extends Post {
+	
+	private Date createBeginDate;
+	
+	private Date postEndDate;
+	
+	
+
+}
