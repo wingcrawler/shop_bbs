8c340c005174c5c8c8b704a985c724052fd59397
Andy <hzwxuex@tairanchina.com>
2017-08-24 09:28:45 +0800

    remove import ibm

--- src/main/java/com/sqe/shop/service/UserService.java
+++ src/main/java/com/sqe/shop/service/UserService.java
@@ -11,7 +11,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.ibm.db2.jcc.b.li;
 import com.sqe.shop.mapper.UserMapper;
 import com.sqe.shop.model.User;
 import com.sqe.shop.util.DateUtil;
