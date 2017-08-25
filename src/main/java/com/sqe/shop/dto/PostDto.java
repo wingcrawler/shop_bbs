2455e780af281baed8fd423e3f012fe8f5259ecc
Andy <winglive@163.com>
2017-08-25 21:48:04 +0800

    Signed-off-by: Andy <winglive@163.com>

--- src/main/java/com/sqe/shop/dto/PostDto.java
+++ src/main/java/com/sqe/shop/dto/PostDto.java
@@ -5,11 +5,25 @@ import java.util.Date;
 import com.sqe.shop.model.Post;
 
 public class PostDto extends Post {
-	
+
 	private Date createBeginDate;
-	
+
 	private Date postEndDate;
-	
-	
+
+	public Date getCreateBeginDate() {
+		return createBeginDate;
+	}
+
+	public void setCreateBeginDate(Date createBeginDate) {
+		this.createBeginDate = createBeginDate;
+	}
+
+	public Date getPostEndDate() {
+		return postEndDate;
+	}
+
+	public void setPostEndDate(Date postEndDate) {
+		this.postEndDate = postEndDate;
+	}
 
 }
