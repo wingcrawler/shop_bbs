4afcecb5b6a1ce2d0fe3ecc6a2d473601fdfcec6
dulz1991 <1025406812@qq.com>
2017-09-06 00:39:46 +0800

    分页bug

--- src/main/webapp/backendstyle/js/jquery.page.js
+++ src/main/webapp/backendstyle/js/jquery.page.js
@@ -30,19 +30,24 @@
 						obj.append('<span class="disabled">'+args.prevPage+'</span>');
 					}
 				}
-				if(args.current != 1 && args.current >= 4 && args.pageCount != 4){
+				
+				if(args.current != 1 && args.current >= 5 && args.pageCount != 5){
 					obj.append('<a href="javascript:;" class="tcdNumber" onClick="'+args.fnName+'(1)">'+1+'</a>');
-				}
-				if(args.current-2 > 2 && args.current <= args.pageCount && args.pageCount > 5){
 					obj.append('<span>...</span>');
 				}
-				var start = args.current -2,end = args.current+2;
-				if((start > 1 && args.current < 4)||args.current == 1){
-					end++;
+				/*if(args.current > args.pageCount/2 && args.pageCount > 5){
+					obj.append('<span>...</span>');
+				}*/
+				
+				var start = 1;
+				var end = 5;
+				if(args.current >= 5){
+					start += args.current-4;
+					end += args.current-4;
 				}
-				if(args.current > args.pageCount-4 && args.current >= args.pageCount){
+				/*if(args.current > args.pageCount-4 && args.current >= args.pageCount){
 					start--;
-				}
+				}*/
 				for (;start <= end; start++) {
 					if(start <= args.pageCount && start >= 1){
 						if(start != args.current){
@@ -52,12 +57,18 @@
 						}
 					}
 				}
-				if(args.current + 2 < args.pageCount - 1 && args.current >= 1 && args.pageCount > 5){
+				
+				if(parseInt(args.current) < parseInt(args.pageCount)-1 && parseInt(args.pageCount)>5){
 					obj.append('<span>...</span>');
-				}
-				if(args.current != args.pageCount && args.current < args.pageCount -2  && args.pageCount != 4){
 					obj.append('<a href="javascript:;" class="tcdNumber" onClick="'+args.fnName+'('+args.pageCount+')">'+args.pageCount+'</a>');
 				}
+				/*if(args.current <= args.pageCount/2 && args.pageCount > 5){
+					obj.append('<span>...</span>');
+				}*/
+				/*if(args.current != args.pageCount && args.current < args.pageCount -2  && args.pageCount != 4){*/
+					/*obj.append('<a href="javascript:;" class="tcdNumber" onClick="'+args.fnName+'('+args.pageCount+')">'+args.pageCount+'</a>');*/
+				/*}*/
+				
 				if(args.current < args.pageCount){
 					if(args.nextPage==undefined){
 						obj.append('<a href="javascript:;" class="nextPage" onClick="'+args.fnName+'('+(parseInt(args.current)+parseInt(1))+')">></a>');
