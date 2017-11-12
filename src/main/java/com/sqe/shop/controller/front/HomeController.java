96c90d9e2b76ccbaedf69dfdaea93540bb5695b3
Andy <winglive@163.com>
2017-11-12 23:27:40 +0800

    增加商城首页接口

--- src/main/java/com/sqe/shop/controller/front/HomeController.java
+++ src/main/java/com/sqe/shop/controller/front/HomeController.java
@@ -1,18 +1,24 @@
 package com.sqe.shop.controller.front;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sqe.shop.common.Constants;
 import com.sqe.shop.controller.base.BaseFrontController;
+import com.sqe.shop.model.Advertisement;
+import com.sqe.shop.model.News;
+import com.sqe.shop.model.Product;
 import com.sqe.shop.model.ProductType;
 import com.sqe.shop.service.AdvertisementService;
 import com.sqe.shop.service.ImageService;
@@ -22,6 +28,7 @@ import com.sqe.shop.service.ProductTypeService;
 import com.sqe.shop.service.biz.BizHomeService;
 import com.sqe.shop.service.cached.CachedService;
 import com.sqe.shop.util.PageUtil;
+import com.sqe.shop.util.RegularUtil;
 
 @Controller
 @RequestMapping("/")
@@ -63,6 +70,97 @@ public class HomeController extends BaseFrontController {
 		return model;
 	}
 	
+	/**
+	 * 商城首页轮播图
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/h5/advertisement", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> advertisement() {
+        Map<String, Object> resMap = this.responseOK1("");		
+		//轮播图
+		Advertisement advertisement = new Advertisement();
+		advertisement.setType(Constants.AD_TYPE_LB);
+		advertisement.setDevice(Constants.AD_DEVICE_PC);
+		PageUtil<Advertisement> adPage = advertisementService.getBeanListByParm(advertisement, 1, -1);
+		resMap.put("adList", adPage.getList());		
+		return resMap;
+	}
+	
+	/**
+	 * 商城首页热门资讯
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/h5/news", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> hotNews() {
+        Map<String, Object> resMap = this.responseOK1("");		
+		//热门资讯
+        //3个新闻资讯
+      		PageUtil<Map<String, Object>> newPage = newsService.getMapListByParm(new News(), 1, 3);
+      		if(newPage.getList()!=null && !newPage.getList().isEmpty()){
+      			String newsTitle = "";
+      			String newsContent = "";
+      			for(Map<String, Object> map : newPage.getList()){
+      				newsTitle = map.get("newsTitle").toString();
+      				if(newsTitle.length()>10){
+      					map.put("newsShortTitle", newsTitle.substring(0, 9)+"...");
+      				} else {
+      					map.put("newsShortTitle", newsTitle);
+      				}
+      				
+      				newsContent = map.get("newsContent").toString();
+      				newsContent = RegularUtil.Html2Text(newsContent);
+      				if(newsContent.length()>100){
+      					newsContent = newsContent.substring(0,100) + "...";
+      					map.put("newsContent", newsContent);
+      				} else {
+      					map.put("newsContent", newsContent);
+      				}
+      				
+      			}
+      		}
+      		resMap.put("newsList", newPage.getList());
+      		
+      		return resMap;
+	}
+	
+	
+	/**
+	 * 商城首页产品分类
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/h5/category", method = RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> category() {
+        Map<String, Object> resMap = this.responseOK1("");	
+        Map<String, Object> parm = new HashMap<String, Object>();
+        parm.put("orderby", "type_rank asc");
+		parm.put("typeLevel", "1");
+		PageUtil<Map<String, Object>> pageUtil = productTypeService.getMapListByParm(parm,1,6);
+		resMap.put("category", pageUtil.getList());		
+		return resMap;
+	}
+	
+	
+	/**
+	 * 商城首页根据产品分类回去产品列表（10个）
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/h5/products", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> ProductsByType(@RequestParam(name="typeId") Long typeId) {
+        Map<String, Object> resMap = this.responseOK1("");	
+        Product product=new Product();
+        product.setProductTypeId(typeId);
+		PageUtil<Map<String, Object>> pageUtil = productService.getHotProductList(product, 1, 10);
+		resMap.put("hotproducts", pageUtil.getList());		
+		return resMap;
+	}
 	
 	/**
 	 * 关于我们
@@ -120,6 +218,9 @@ public class HomeController extends BaseFrontController {
 		resMap.put("list", page.getList());
 		return resMap;
 	}
+	
+	
+	
 	
 	/**
 	 * 刷新缓存
